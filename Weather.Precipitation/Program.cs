7cbcd1f3ae342d7cc6b274e814f7f736955e8870
Jakob Dennryd <jakob.dennryd@utb.ecutbildning.se>
2022-12-08 18:01:54 +0100

    access to precip service

--- Weather.Precipitation/Program.cs
+++ Weather.Precipitation/Program.cs
@@ -1,13 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Weather.Precipitation.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<PrecipDbContext>(opts =>
+{
+    opts.EnableSensitiveDataLogging();
+    opts.EnableDetailedErrors();
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+}, ServiceLifetime.Transient
+);
 
 var app = builder.Build();
 
-app.MapGet("/obeservation/{zip}", (string zip, [FromQuery] int? days)=>
+
+
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) =>
 {
-    return Results.Ok(zip);
+    if (days == null || days < 1 || days > 30)
+    {
+        return Results.BadRequest("Please provide number of days between 1 and 30");
+    }
+    var dateStart = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Precipitation
+    .Where(precip => precip.ZipCode == zip && precip.CreatedOn > dateStart).ToListAsync();
+
+    return Results.Ok(results);
+
+
 });
 
 
